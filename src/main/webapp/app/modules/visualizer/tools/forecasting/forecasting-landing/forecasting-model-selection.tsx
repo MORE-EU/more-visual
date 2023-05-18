130f916e294ca80e1814536690b80ff3c472547c
Konstantinos Kozanis <kon.kozanis@gmail.com>
2023-05-18 16:39:37 +0300

    changes to the landing page

--- src/main/webapp/app/modules/visualizer/tools/forecasting/forecasting-landing/forecasting-model-selection.tsx
+++ src/main/webapp/app/modules/visualizer/tools/forecasting/forecasting-landing/forecasting-model-selection.tsx
@@ -9,22 +9,30 @@ import AddIcon from '@mui/icons-material/Add';
 import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
+import TableContainer from '@mui/material/TableContainer';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import TableBody from '@mui/material/TableBody';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const savedModels = ['model1', 'model2', 'model3', 'model4', 'model5'];
 
-const ForecastingModelSelection = (props) => {
-    const handleNewTrain = () => {
-        props.setNewTrain(true);
-    }
+const ForecastingModelSelection = props => {
+  const handleNewTrain = () => {
+    props.setNewTrain(true);
+  };
   return (
-    <Grid sx={{ height: '100%', width: '100%', scroll: 'auto', display: 'flex', flexDirection: 'column' }}>
+    <Grid sx={{ height: '100%', width: '100%', scroll: 'auto', display: 'flex', flexDirection: 'column', rowGap: 2 }}>
       <Grid sx={{ width: '80%', borderBottom: '1px solid rgba(0,0,0,0.3)', textAlign: 'center', m: 'auto' }}>
         <Typography variant="subtitle1" fontSize={20}>
           Saved Models
         </Typography>
       </Grid>
-      <Grid sx={{ width: '40%', textAlign: 'center', m: 'auto', rowGap: 1, display: 'flex', flexDirection: 'column' }}>
-        {savedModels.map((modelName) => (
+      <Grid sx={{ width: '60%', textAlign: 'center', m: 'auto', rowGap: 1, display: 'flex', flexDirection: 'column', overflowY: "auto", pb: 4 }}>
+        {/* {savedModels.map((modelName) => (
           <Grid key={modelName} sx={{ width: '100%', display: 'flex', bgcolor: grey[200], borderRadius: 10, alignItems: 'center' }}>
             <Typography variant="subtitle1" fontSize={16} sx={{ pl: 3 }}>
               {modelName}
@@ -47,7 +55,63 @@ const ForecastingModelSelection = (props) => {
           <Typography variant="subtitle1" fontSize={14}>
             Train a new model
           </Typography>
-        </Button>
+        </Button> */}
+        <TableContainer component={Paper}>
+          <Table aria-label="caption table" size="small">
+            <caption style={{padding: 0}}>
+              <Button
+                variant="text"
+                // sx={{
+                //   borderRadius: 10,
+                //   fontSize: 4,
+                //   textDecoration: 'none',
+                //   justifyContent: 'flex-start',
+                //   alignItems: 'center',
+                //   bgcolor: grey[400],
+                //   color: grey[800],
+                // }}
+                onClick={handleNewTrain}
+              >
+                <AddIcon sx={{fontSize: 16}}/>
+                <Typography variant="subtitle1" fontSize={12}>
+                  Train a new model
+                </Typography>
+              </Button>
+            </caption>
+            <TableHead>
+              <TableRow>
+                <TableCell>Model Name</TableCell>
+                <TableCell align="right">Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {savedModels.map(modelName => (
+                <TableRow key={modelName}>
+                  <TableCell component="th" scope="row">
+                    {modelName}
+                  </TableCell>
+                  <TableCell align="right">
+                    <Tooltip title="Retrain Model">
+                      <IconButton>
+                        <ModelTrainingIcon />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Predict">
+                      <IconButton>
+                        <QueryStatsIcon />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Delete">
+                      <IconButton>
+                        <DeleteIcon />
+                      </IconButton>
+                    </Tooltip>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Grid>
     </Grid>
   );
