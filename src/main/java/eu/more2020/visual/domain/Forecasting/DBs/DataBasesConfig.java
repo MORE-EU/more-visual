e892420f4f2c328bd9ae8574505feaa34e466f1c
Konstantinos Kozanis <kon.kozanis@gmail.com>
2023-09-13 11:34:26 +0300

    changes to dbs info

---
+++ src/main/java/eu/more2020/visual/domain/Forecasting/DBs/DataBasesConfig.java
@@ -0,0 +1,99 @@
+package eu.more2020.visual.domain.Forecasting.DBs;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataBasesConfig {
+
+    private String token;
+
+    private String bucket;
+
+    private String org;
+
+    private String mongo_uri;
+
+    private String mongo_db_name;
+
+    private String mongo_user_name;
+
+    private String mongo_user_password;
+
+    public DataBasesConfig(String token, String bucket, String org, String mongo_uri, String mongo_db_name,
+            String mongo_user_name, String mongo_user_password) {
+        this.token = token;
+        this.bucket = bucket;
+        this.org = org;
+        this.mongo_uri = mongo_uri;
+        this.mongo_db_name = mongo_db_name;
+        this.mongo_user_name = mongo_user_name;
+        this.mongo_user_password = mongo_user_password;
+    }
+
+    public DataBasesConfig() {
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getBucket() {
+        return bucket;
+    }
+
+    public void setBucket(String bucket) {
+        this.bucket = bucket;
+    }
+
+    public String getOrg() {
+        return org;
+    }
+
+    public void setOrg(String org) {
+        this.org = org;
+    }
+
+    public String getMongo_uri() {
+        return mongo_uri;
+    }
+
+    public void setMongo_uri(String mongo_uri) {
+        this.mongo_uri = mongo_uri;
+    }
+
+    public String getMongo_db_name() {
+        return mongo_db_name;
+    }
+
+    public void setMongo_db_name(String mongo_db_name) {
+        this.mongo_db_name = mongo_db_name;
+    }
+
+    public String getMongo_user_name() {
+        return mongo_user_name;
+    }
+
+    public void setMongo_user_name(String mongo_user_name) {
+        this.mongo_user_name = mongo_user_name;
+    }
+
+    public String getMongo_user_password() {
+        return mongo_user_password;
+    }
+
+    public void setMongo_user_password(String mongo_user_password) {
+        this.mongo_user_password = mongo_user_password;
+    }
+
+    @Override
+    public String toString() {
+        return "DataBasesConfig [token=" + token + ", bucket=" + bucket + ", org=" + org + ", mongo_uri=" + mongo_uri
+                + ", mongo_db_name=" + mongo_db_name + ", mongo_user_name=" + mongo_user_name + ", mongo_user_password="
+                + mongo_user_password + "]";
+    }
+
+}
